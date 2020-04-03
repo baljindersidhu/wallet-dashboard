7cb44baebca40bb0bb20d56b8e83fcaa7b90076b
Baljinder Singh Sidhu <Baljinder.S.Sidhu@aexp.com>
2020-04-04 00:38:37 +0530

    Updates

---
+++ precache-manifest.a3132e5381627ef888031347d4650528.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a9d4705d2798c4f590ab55c9da3a9f1",
+    "url": "/wallet-dashboard/index.html"
+  },
+  {
+    "revision": "a95d9231942080bf8d7d",
+    "url": "/wallet-dashboard/static/css/main.e2c30be2.chunk.css"
+  },
+  {
+    "revision": "9daf19d257b02206d1ae",
+    "url": "/wallet-dashboard/static/js/2.bd41938e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/wallet-dashboard/static/js/2.bd41938e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a95d9231942080bf8d7d",
+    "url": "/wallet-dashboard/static/js/main.88fd7eb2.chunk.js"
+  },
+  {
+    "revision": "8da1683b87ede0a7ac1d",
+    "url": "/wallet-dashboard/static/js/runtime-main.bee30971.js"
+  }
+]);
